f143f6a98720a6b6b2db8cbe227b4c914062ad1e
Syed Ovais Akhtar <ovaisakhtar97@gmail.com>
2020-04-18 02:51:25 +0500

    Initial Commit

---
+++ app/src/main/java/com/org/androidappdevelopmentpractice/ui/auth/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.org.androidappdevelopmentpractice.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.org.androidappdevelopmentpractice.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
